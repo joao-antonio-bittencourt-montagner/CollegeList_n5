df78a3601a956e9fb8afbbdee4d1640ed62771d8
joao-antonio-bittencourt-montagner <joaoantoniob.montagner@gmail.com>
2024-06-24 09:43:45 -0400

    Update Questão 50.py

--- Atividade 5 - Jean/Questão 50.py
+++ Atividade 5 - Jean/Questão 50.py
@@ -1,29 +1,64 @@
-n = str(input('Digite um número romano: ')).upper()
-l = []
-c = c1 = 0
-for i in range (len(n)):
-    l.append(n[i])
+import random
+d = 3
+dd = d - 1
+l = [[random.randint(1,1) for i in range(d)]for i in range (d)]
+rs = j = jj = 0
+lr = []
+lr1 = {}
 
-while c1 < len(l):
-    if l[c1] == 'I' and l[c1+1] == 'V':
-        c += 4
-        c1 += 2
-        print('sim4')
-
-    elif l[c1] == 'I' and l[c1+1] == 'X':
-        c += 9
-        c1 += 2
+for i in range(d):
+    print(l[i])
     
-    elif l[c1] == 'I':
-        c += 1
-        c1 += 1
-        print('sim1')
-    elif l[c1] == 'V':
-        c += 5
-        c1 += 1
-        print('sim5')
-    elif l[c1] == 'X':
-        c += 10
-        c1 += 1
 
-print(c)
+for i in range(d):
+    for ii in range(d):
+        rs += l[i][ii]
+    
+    lr.append(rs)
+    rs = 0
+lr1['Linhas'] = lr[:]
+lr.clear()
+
+
+for i in range(d):
+    for ii in range(d):
+        rs += l[ii][i]
+    
+    lr.append(rs)
+    rs = 0
+lr1['Colunas'] = lr[:]
+lr.clear()
+
+
+for i in range(d):
+    for ii in range(d):
+        if i == ii:
+            rs += l[i][ii]
+    
+lr.append(rs)
+rs = 0
+lr1['Diagonal-D'] = lr[:]
+lr.clear()
+
+
+for i in range(d):
+    for ii in range(d):
+        if i == ii:
+            rs += l[i][dd]
+            print(dd)
+            dd -= 1
+            
+lr.append(rs)
+rs = 0
+lr1['Diagonal-E'] = lr[:]
+lr.clear()
+print(lr1)
+
+c = 0
+if lr1['Linhas'] == lr1['Colunas']:
+    if lr1['Diagonal-D'] == lr1['Diagonal-E']:
+        for e in lr1['Linhas']:
+            if e in lr1['Diagonal-D']:
+                c += 1
+            if c == d:
+                print('Cocoricó')
