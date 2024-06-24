ac8afa33a6a9d95f2324973593b98feb7d679e1a
joao-antonio-bittencourt-montagner <joaoantoniob.montagner@gmail.com>
2024-06-24 17:10:41 -0400

    Update Questão 49.py

--- Atividade 5 - Jean/Questão 49.py
+++ Atividade 5 - Jean/Questão 49.py
@@ -1,10 +1,32 @@
-n = str(input('Digite uma seqência de números que representam um intervalo: '))
-l= ['1','2','3','4','5','6','7','8','9']
-n2 = []
+n = str(input('Digite uma sequência de números que representam um intervalo por -: '))
+l= []
+e = d = c = 0
 for i in range(len(n)):
-    for ii in range(len(l)):
-        if n[i] == l[ii]:
-            n2.append(l[ii])
+    print(i)
+    if n[i] == '-':
+        e = n[i-1]   
+        d = n[i+1]
+        e = int(e)
+        d = int(d)
+        print(e)
+        print(d)
+        while e < d:
+             l.append(e)
+             e += 1
+        l.append(d)
+        
+    if n[i] not in l and n[i] != '-' and n[i] != ',':
+        l.append(n[i])
+l2 = []
+for i in range(len(l)):
+    c = l[i]
+    c = int(c)
+    if c not in l2:
+        l2.append(c)
+# 1-3,5,6,7-9
+print(n)
+print(l)
+print(l2)
 
-for i in range(len(n2)):
-    n2 = int(n2[i])
+
+# 1,5,6-8,9
